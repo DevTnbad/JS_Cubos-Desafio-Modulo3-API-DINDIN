448fb3934030cd0742055ec7ac1ce0af7cacdd17
DevTnbad <dev.tnbad@gmail.com>
2023-10-17 11:57:54 -0300

    ajuste na porta

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(rotas);
-
-app.listen(process.env.PORT, () => console.log(`Servidor Desafio 3 Subiu na porta:${process.env.PORT}`))
+const porta = process.env.PORT || 3000
+app.listen(porta, () => console.log(`Servidor Desafio 3 Subiu na porta:${porta}`))
